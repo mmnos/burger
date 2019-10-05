a1b9262d3e96ed5a82132a56a4737056bfe0d0b9
Michael Nos <mnos9897@gmail.com>
2019-10-05 14:35:32 -0400

    Added a Procfile

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,12 @@
 const mysql = require("mysql");
+require('dotenv').config();
 
 const connection = mysql.createConnection({
-  host: "localhost",
+  host: process.env.DB_HOST,
   port: 3306,
-  user: "root",
-  password: "password",
-  database: "burgers_db"
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB
 });
 
 connection.connect(function(err) {
